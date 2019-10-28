37ecbbc652a8b698d48ac6c2cd0f0aec6abbea06
Dominique Maack <dominiquemaack@Dominiques-MacBook-Pro.local>
2019-10-28 16:21:51 -0600

    GET users endpoint

--- index.js
+++ index.js
@@ -13,21 +13,27 @@ server.post('/api/users', (req, res) => {
     const { name, bio } =  req.body
 
     if( !name || !bio ) {
-        res.status(400).json({ error: 'Please provide a name and bio for your user'})
+        res.status(400).json({ error: 'Please provide name and bio for the user.' })
     } else {
         db.insert(req.body)
         .then(user => {
             res.status(201).json(user)
         })
         .catch(() => {
-            res.status(500).json({ error: 'Error saving the user to our database'})
+            res.status(500).json({ error: 'There was an error while saving the user to the database' })
         })
     }
 })
 
-// server.get('/api/users', (req, res) => {
-
-// })
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then(users => {
+        res.status(201).json(users)
+    })
+    .catch(() => {
+        res.status(500).json({ error: 'The users information could not be retrieved.'})
+    })
+})
 
 // server.get('/api/users/:id', (req, res) => {
 
