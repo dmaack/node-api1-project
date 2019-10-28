d1207b7461643a2a6aae0800559a9609390927aa
Dominique Maack <dominiquemaack@Dominiques-MacBook-Pro.local>
2019-10-28 16:27:30 -0600

    GET users by ID endpoint

--- index.js
+++ index.js
@@ -35,9 +35,21 @@ server.get('/api/users', (req, res) => {
     })
 })
 
-// server.get('/api/users/:id', (req, res) => {
+server.get('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
 
-// })
+    db.findById(userId)
+    .then(user => {
+        if(user) {
+            res.status(200).json(user)
+        } else {
+            res.status(404).json({ error: 'The user with the specified ID does not exist.'})
+        }
+    })
+    .catch(() => {
+        res.status(500).json({ error: 'The user information could not be retrieved.'})
+    })
+})
 
 // server.delete('/api/users/:id', (req, res) => {
 
